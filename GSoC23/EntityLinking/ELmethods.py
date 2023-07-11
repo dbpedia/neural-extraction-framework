20c1658dc3bee94e7de14021c0c9ba8d486f00b7
Aakash Thatte <aathatte2002@gmail.com>
2023-07-12 00:04:09 +0530

    Move utility code for redis-db

--- GSoC23/EntityLinking/ELmethods.py
+++ GSoC23/EntityLinking/ELmethods.py
@@ -22,12 +22,12 @@ def EL_DBpedia_lookup(query: str, max_results: int):
     resources = [d['resource'][0] for d in docs]
     return resources
 
-def EL_DBpedia_spotlight(query: str, nlp: spacy.lang.en.English):
+def EL_DBpedia_spotlight(query: str, nlp):
     """Perform entity linking using DBpedia spotlight using spacy component.
 
     Args:
         query (str): The entity mention that you want to link to a resource in dbpedia.
-        nlp (spacy.lang.en.English): Spacy model for english language added with a component 
+        nlp : Spacy model for english language added with a component 
         called `dbpedia_spotlight`.
 
         The nlp pipeline should output something similar to below.
@@ -67,35 +67,7 @@ def EL_redis_db(query: str,
         DataFrame: A pandas dataframe with candidate entity scores and other info. We can then
         select only the candidate URIs.
     """
+    from GSoC23.EntityLinking.el_utils import lookup
     return lookup(term=query, 
     redis_client_forms = redis_client_forms,
     redis_client_redir = redis_client_redir)
-
-def calculate_redirect(source, client):
-    result = client.get(source)
-    if result is None:
-        return source if type(source) is str else source.decode('utf-8')
-    return calculate_redirect(result, client)
-
-def query(surface_form, client):
-    raw = client.hgetall(surface_form)
-    if len(raw) == 0:
-        return pd.DataFrame(columns=['entity', 'support', 'score'])
-    
-    out = []
-    for label, score in raw.items():
-        out.append({'entity': label.decode('utf-8'), 'support': int(score)})
-    df_all = pd.DataFrame(out)
-    df_all['score'] = df_all['support'] / df_all['support'].max()
-    
-    return df_all.sort_values(by='score', ascending=False).reset_index(drop=True)
-
-def lookup(term, top_k=5, thr=0.01, redis_client_forms=None, redis_client_redir=None):
-    df_temp = query(term, redis_client_forms)
-#     display(df_temp)
-    df_temp['entity'] = df_temp['entity'].apply(lambda x: calculate_redirect(x, redis_client_redir))
-    if len(df_temp) == 0:
-        return pd.DataFrame(columns=['entity', 'support', 'score'])
-    df_final = df_temp.groupby('entity').sum()[['support']]
-    df_final['score'] = df_final['support'] / df_final['support'].max()
-    return df_final[df_final['score'] >= thr].sort_values(by='score', ascending=False)[:top_k]
