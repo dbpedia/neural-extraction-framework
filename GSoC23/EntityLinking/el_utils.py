c5226298aa8e1deef1aba0a4a74cebaf10fd827e
Aakash Thatte <aathatte2002@gmail.com>
2023-07-12 00:05:07 +0530

    Create a EL utils file for EL utilities

--- GSoC23/EntityLinking/el_utils.py
+++ GSoC23/EntityLinking/el_utils.py
@@ -1,5 +1,48 @@
 import pandas as pd
 
+def get_majority_vote(candidate_list):
+    """Perform majority vote over a list of candidate entities
+    based on frequency.
+
+    Initially, we are using frequency to do the voting. 
+    But later on, we can also do a weighted voting, 
+    by weighing each candidate based on the method which returned it,
+    thereby giving more importance to more accurate methods.
+
+    Args:
+        candidate_list : List of candidate entities/resources.
+
+    Returns:
+        Returns a resource/entity URI
+    """
+    candidate_entity = max(set(candidate_list), key=candidate_list.count)
+    return candidate_entity
+
+def convert_sentence_for_genre_model(sentence, entity):
+    """A function to provide annotated form of the sentence.
+    The sentence we provide here should first be passed through
+    the NER system to get all entities in it. Then, with each entity
+    individually, we pass it to this function. And we pass the result of 
+    this function to the GENRE entity disambiguation model.
+
+    Args:
+        sentence (_type_): A given sentence
+        entity (_type_): A dictionary containing the entity text
+        entity span and other such info
+    """
+    start = entity['start']
+    end = entity['end']
+
+    # We need to add [START_ENT] tag before the entity and [END_ENT] tag 
+    # after the entity we want to link and disambiguate
+    result_sentence = sentence[:start]
+    result_sentence+= '[START_ENT] '
+    result_sentence+= sentence[start:end]
+    result_sentence+= ' [END_ENT] '
+    result_sentence+= sentence[end:]
+    result_sentence = result_sentence.strip()
+    return result_sentence
+
 def calculate_redirect(source, client):
     result = client.get(source)
     if result is None:
