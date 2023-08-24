4dce8f48b9913440e2d3858026fffb6ea4a09118
Aakash Thatte <[aathatte2002@gmail.com](mailto:aathatte2002@gmail.com)>
2023-08-25 00:35:22 +0530

    Perform black formatting

--- GSoC23/CoreferenceResolution/FCOREF.py
+++ GSoC23/CoreferenceResolution/FCOREF.py
@@ -5,16 +5,15 @@ nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe(
     "fastcoref",
     config={
-        'model_architecture': 'LingMessCoref', 
-        'model_path': 'biu-nlp/lingmess-coref', 
-        'device': 'cpu'}
-    )
+        "model_architecture": "LingMessCoref",
+        "model_path": "biu-nlp/lingmess-coref",
+        "device": "cpu",
+    },
+)
+
 
 def get_coref_resolved_text(text):
     global nlp
-    doc = nlp(
-    text,
-    component_cfg={"fastcoref": {'resolve_text': True}}
-    )
+    doc = nlp(text, component_cfg={"fastcoref": {"resolve_text": True}})
 
     return doc._.resolved_text
