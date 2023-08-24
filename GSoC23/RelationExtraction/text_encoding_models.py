1037bc7e21a68203036d693c0587e41861871a70
Aakash Thatte <[aathatte2002@gmail.com](mailto:aathatte2002@gmail.com)>
2023-08-25 00:37:43 +0530

    Perform black formatting

--- GSoC23/RelationExtraction/text_encoding_models.py
+++ GSoC23/RelationExtraction/text_encoding_models.py
@@ -1,9 +1,12 @@
 from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+
 
 def get_sentence_transformer_model(model_name):
     model = SentenceTransformer(model_name_or_path=model_name)
     return model
+
 
 def get_embeddings(labels, sent_tran_model):
     embeddings = sent_tran_model.encode(labels)
