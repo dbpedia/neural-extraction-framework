310b07fce64867b30e4c83647f04c21293254a81
Aakash Thatte <[aathatte2002@gmail.com](mailto:aathatte2002@gmail.com)>
2023-08-25 00:37:43 +0530

    Perform black formatting

--- GSoC23/RelationExtraction/methods.py
+++ GSoC23/RelationExtraction/methods.py
@@ -1,22 +1,23 @@
 from GSoC23.RelationExtraction.rebel import extract_relations_rebel
 from GSoC23.RelationExtraction.re_utils import get_triple_from_triple
 
+
 def get_triples_from_sentence_using_rebel(sentence, rebel_model, rebel_tokenizer):
-    sent_triples =  extract_relations_rebel(rebel_model, rebel_tokenizer, text=sentence)
+    sent_triples = extract_relations_rebel(rebel_model, rebel_tokenizer, text=sentence)
     triples = []
-    
+
     for i in range(len(sent_triples)):
         subject, relation, objct, score = sent_triples.iloc[i].values
         triple = get_triple_from_triple(subject, relation, objct, sentence)
         triples.append(
-        {
-            "subject":subject,
-            "predicate":relation,
-            "object":objct,
-            "subject_URI": triple[0],
-            "predicate_URI":triple[1],
-            "object_URI":triple[2],
-            "relation_confidence_score":score,
-        }
+            {
+                "subject": subject,
+                "predicate": relation,
+                "object": objct,
+                "subject_URI": triple[0],
+                "predicate_URI": triple[1],
+                "object_URI": triple[2],
+                "relation_confidence_score": score,
+            }
         )
     return triples
--- GSoC23/RelationExtraction/re_utils.py
+++ GSoC23/RelationExtraction/re_utils.py
@@ -3,36 +3,49 @@ import pickle
 import logging
 from GSoC23.EntityLinking.methods import EL_GENRE
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-from GSoC23.RelationExtraction.relation_similarity import load_key_vector_model_from_file
-from GSoC23.RelationExtraction.text_encoding_models import get_sentence_transformer_model
+from GSoC23.RelationExtraction.relation_similarity import (
+    load_key_vector_model_from_file,
+)
+from GSoC23.RelationExtraction.text_encoding_models import (
+    get_sentence_transformer_model,
+)
 from GSoC23.RelationExtraction.relation_similarity import ontosim_search
 from GSoC23.EntityLinking.el_utils import annotate_sentence
 
-with open("config.json",'r') as config_file:
+with open("config.json", "r") as config_file:
     config = json.load(config_file)
 
-label_embeddings_file = config['file_paths']['label_embeddings_file']
+label_embeddings_file = config["file_paths"]["label_embeddings_file"]
 gensim_model = load_key_vector_model_from_file(label_embeddings_file)
 
-tbox_pickle_file = config['file_paths']['tbox_pickle_file']
-labels_pickle_file = config['file_paths']['labels_pickle_file']
+tbox_pickle_file = config["file_paths"]["tbox_pickle_file"]
+labels_pickle_file = config["file_paths"]["labels_pickle_file"]
+
 
 def get_pickle_object(file):
     """This function can be used to load the labels.pkl
     and tbox.pkl files.
     """
-    with open(file,"rb") as f:
+    with open(file, "rb") as f:
         return pickle.load(f)
-    
-labels=get_pickle_object(labels_pickle_file)
-tbox=get_pickle_object(tbox_pickle_file)
 
-encoder_model = get_sentence_transformer_model(model_name=config['model_names']['encoder_model'])
+
+labels = get_pickle_object(labels_pickle_file)
+tbox = get_pickle_object(tbox_pickle_file)
+
+encoder_model = get_sentence_transformer_model(
+    model_name=config["model_names"]["encoder_model"]
+)
 logging.info("Encoder model loaded")
 
-genre_tokenizer = AutoTokenizer.from_pretrained(config['model_names']['genre_tokenizer'])
-genre_model = AutoModelForSeq2SeqLM.from_pretrained(config['model_names']['genre_model']).eval()
+genre_tokenizer = AutoTokenizer.from_pretrained(
+    config["model_names"]["genre_tokenizer"]
+)
+genre_model = AutoModelForSeq2SeqLM.from_pretrained(
+    config["model_names"]["genre_model"]
+).eval()
 logging.info("GENRE model loaded")
+
 
 def get_triple_from_triple(sub, relation, obj, sentence):
     """Give subject, relation, object in natural language form,
@@ -46,18 +59,21 @@ def get_triple_from_triple(sub, relation, obj, sentence):
     Returns:
         tuple: A tuple of entity and predicate URIs
     """
-    
+
     subject_entity = EL_GENRE(
-        annotate_sentence(sentence, sub), genre_model, genre_tokenizer)[0]
-    subject_entity = "https://dbpedia.org/resource/"+"_".join(subject_entity.split())
-    
+        annotate_sentence(sentence, sub), genre_model, genre_tokenizer
+    )[0]
+    subject_entity = "https://dbpedia.org/resource/" + "_".join(subject_entity.split())
+
     object_entity = EL_GENRE(
-        annotate_sentence(sentence, obj), genre_model, genre_tokenizer)[0]
-    object_entity = "https://dbpedia.org/resource/"+"_".join(object_entity.split())
-    
-    predicates, label, score = ontosim_search(
-        relation, gensim_model, encoder_model, tbox).iloc[0].values
-    
+        annotate_sentence(sentence, obj), genre_model, genre_tokenizer
+    )[0]
+    object_entity = "https://dbpedia.org/resource/" + "_".join(object_entity.split())
+
+    predicates, label, score = (
+        ontosim_search(relation, gensim_model, encoder_model, tbox).iloc[0].values
+    )
+
     predicate = None
     for p in predicates:
         if p.split("/")[-1][0].islower():
@@ -65,4 +81,3 @@ def get_triple_from_triple(sub, relation, obj, sentence):
             break
     # can also return this - (subject_entity, (predicate, score), object_entity)
     return (subject_entity, predicate, object_entity)
-
